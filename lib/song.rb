495946cd60b777865e021ab7a57cbc4fb59c5c33
Aiden Marques <aiden.marques94@gmail.com>
2020-10-01 00:47:11 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,40 @@
+require 'pry'
+class Song
+  
+@@count = 0 
+@@artist_count={}
+@@genre_count={}
+@@artists = []
+@@genres = []
+
+    attr_accessor :name, :artist, :genre  
+    
+    def initialize(name,artist,genre)
+      
+      @name=name
+      @artist = artist
+      @genre=genre
+      @@count+= 1
+      @@artists << artist
+      @@genres << genre
+      
+    end
+    
+    def self.count
+      @@count
+    end
+    
+    def self.artists
+      @@artists.uniq 
+    end
+    
+    def self.genres
+      @@genres.uniq 
+    end 
+    
+    def genre_count
+      
+      self.each do |genres,index| @@genre_count << genres + index
+    end 
+    end
+end 
